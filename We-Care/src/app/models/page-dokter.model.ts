2a59659b3c0503ecbe9b68370e6d79f56543be1b
Josh-IceBear <inkiriwangjosh21@gmail.com>
2021-04-30 14:33:40 +0700

    Clear

--- We-Care/src/app/models/page-dokter.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Dokter {
-    // Info Pribadi Dokter
-    id_dokter: string;
-    nama_dokter: string;
-    tempat_lahir: string;
-    tanggal_lahir: string;
-    imageUrl: string;
-}
-
